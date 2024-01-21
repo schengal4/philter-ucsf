e998bb81c6a38490a64767138f64071b606a88ec
Venkata Sai Suraj Chengalvala <55936921+schengal4@users.noreply.github.com>
2024-01-20 21:56:35 -0500

    Feature Add

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 note = """APP HAS ISSUES WITH PYTHON 3.12, USE PYTHON 3.10 INSTEAD"""
 
 import streamlit as st
-from utils import upload_file
+from utils import text_from_pdf_file, read_docx, text_from_pdf_file_path, upload_file
 import uuid
 
 
@@ -10,7 +10,7 @@ import time
 import os
 
 
-def deidentify(uploaded_file_name, uploaded_file_text):
+def deidentify(uploaded_file_text, uploaded_file_name = "note_"):
     """
     Source of core functionality: https://github.com/BCHSI/philter-ucsf
     """
@@ -50,29 +50,93 @@ def deidentify(uploaded_file_name, uploaded_file_text):
 
         return output
 def initialize_session_state():
-    if "deidentified_file" not in st.session_state:
-        st.session_state["deidentified_file"] = None
+    if "input_choice" not in st.session_state:
+        st.session_state["input_choice"] = None
+    if "prev_uploaded_file" not in st.session_state:
+        st.session_state["prev_uploaded_file"] = None
+    if "downloaded_file" not in st.session_state:
+        st.session_state["downloaded_file"] = False
+    if "deidentified_note" not in st.session_state:
+        st.session_state["deidentified_note"] = None
+
+# This is a test report with fake PHI/PII to test the app.
+EXAMPLE_REPORT = """**Patient Name:** John Doe  
+**Date of Birth:** 02/14/1983  
+**Address:** 123 Main Street, Springfield, IL, 62704  
+**Phone:** (555) 123-4567  
+**Email:** johndoe@example.com  
+**SSN:** 123-45-6789  
+**Insurance ID:** 9876543210  
+**Primary Physician:** Dr. Jane Smith  
+**Date of Visit:** 01/20/2024  
+
+**Medical History:**  
+- Patient has a history of Type 2 Diabetes diagnosed in 2010.
+- Previous surgery: Appendectomy in 2015.
+
+**Medications:**  
+- Metformin 500 mg twice daily.
+- Lisinopril 10 mg once daily.
+
+**Allergies:**  
+- Penicillin
+- Peanuts
+
+**Chief Complaint:**  
+Patient complains of intermittent chest pain for the past week, often occurring during physical activity.
+
+**Examination Findings:**  
+- Blood Pressure: 140/90 mmHg
+- Heart Rate: 78 bpm
+- Height: 5 feet 9 inches
+- Weight: 200 lbs
+- Physical exam unremarkable except for mild tenderness in the chest area upon palpation.
+
+**Assessment and Plan:**  
+- Suspected stable angina.
+- Recommend cardiac stress test and echocardiogram.
+- Increase physical activity, advise to follow a heart-healthy diet.
+- Follow-up appointment scheduled for 02/01/2024 with Cardiology Specialist Dr. Emily Taylor at Springfield Cardiac Care, 456 Health Rd, Springfield, IL, 62704.
+
+**Notes:**  
+Patient expressed concerns about managing new symptoms with current work schedule. Discussed importance of lifestyle modification and regular monitoring of cardiovascular health.
+
+"""
 def main():
     initialize_session_state()
     st.title("Deidentify PHI in your file")
     st.markdown("This app uses the [Philter](https://github.com/BCHSI/philter-ucsf) library to deidentify PHI in your file.")
     st.markdown("""
-    ## Instructions
-    1. Upload a file in .txt, .pdf, or .docx format.  
-    2. After a couple seconds, the file will be deidentified and the output will be displayed.  
-    """)
-    st.markdown("## Upload your file")
-    uploaded_file_name, uploaded_file_text = upload_file()
-    initial_time = time.time()
-    output = deidentify(uploaded_file_name, uploaded_file_text)
-    if output is not None and output.strip() != "": 
-        st.session_state["deidentified_file"] = output
-    if st.session_state["deidentified_file"] is not None:
-        st.markdown("## Output (Deidentified file)")
-        st.text(st.session_state["deidentified_file"])
-        st.download_button("Download file as .txt", data=st.session_state["deidentified_file"], file_name="deidentified_file.txt")
-    end_time = time.time()
-    time_taken = end_time - initial_time
-    print(time_taken)
+    ### Instructions
+    1. Paste the text of the clinical note/text, or upload a file in .txt, .pdf, or .docx format.  
+    2. After a few seconds, the file will be deidentified and the output will be displayed.  
+    """) 
+    st.markdown("""
+                ### Enter Text or Upload File Below
+                """)
+    input_choice = st.radio("Choose your input method:", ('Paste Text', 'Upload File'), horizontal=True)
+    st.session_state["input_choice"] = input_choice
+    if input_choice == 'Paste Text':
+        text_container = st.empty()
+        report_text = text_container.text_area("Paste your clinical note/text here. After pasting it, press ctrl + Enter and then press 'Deidentify clinical note':")
+    else:
+        _, report_text = upload_file()
+    col1, col2, _ = st.columns([3, 4, 3])
+    deidentify_report_button = col1.button("Deidentify clinical note", type="primary")
+    try_example = col2.button("Try example clinical note")
+    if try_example:
+        if input_choice == 'Paste Text':
+            report_text = text_container.text_area("Paste your clinical note/text here. After pasting it, press ctrl + Enter and then press 'Deidentify clinical note':", value=EXAMPLE_REPORT, key="text_area")
+            st.session_state["deidentified_note"] = deidentify(report_text)
+        else:
+            report_text = EXAMPLE_REPORT
+            st.session_state["deidentified_note"] = deidentify(report_text)
+    if deidentify_report_button and report_text.strip():
+        st.session_state["deidentified_note"] = deidentify(report_text)
+
+    if st.session_state["deidentified_note"]:
+        st.subheader("Deidentified clinical note:")
+        st.text(st.session_state["deidentified_note"])
+        st.download_button("Download file as .txt", data=st.session_state["deidentified_note"], file_name="deidentified_note.txt")
 if __name__ == "__main__":
     main()
